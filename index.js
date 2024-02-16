57f92d274810ff4f6822c361a05113c9d912595e
Satyam2192 <satyam21092@google.com>
2024-02-16 17:06:20 +0530

    ready for deployment

--- index.js
+++ index.js
@@ -51,11 +51,11 @@ app.use('/api/navabr', navbarRouter)
 
 
 
-// app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '/dist')));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-// })
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
